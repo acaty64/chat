a914670c12ef2af9b2209f87385a81beee965534
Ana <ana.arashiro@gmail.com>
2020-11-09 17:39:18 -0500

    first commit

---
+++ app/User.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use App\Access;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    public function __construct($attributes = [])
+    {
+        parent::__construct($attributes);
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+}
---
+++ public/camera.example.index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Camera</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- <link rel="stylesheet" type="text/css" href="main.css"> -->
+	</head>
+	<body>
+		<video id="video" autoplay> Video stream not available</video>
+
+		<script async src="capture.js"></script>
+
+	</body>
+</html>
